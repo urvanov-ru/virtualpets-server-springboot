9259b2a02e2f42092c62498985b4c35b5cd73f3c
Fedor Urvanov <fedor@urvanov.ru>
2024-05-05 14:14:31 +0300

    BookDao organize imports

--- src/main/java/ru/urvanov/virtualpets/server/dao/BookDao.java
+++ src/main/java/ru/urvanov/virtualpets/server/dao/BookDao.java
@@ -1,7 +1,6 @@
 package ru.urvanov.virtualpets.server.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import ru.urvanov.virtualpets.server.dao.domain.Book;
