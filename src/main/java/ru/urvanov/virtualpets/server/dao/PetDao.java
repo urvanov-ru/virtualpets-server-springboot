8f2831313052cb1f222e7d0d474a1426744196bb
Fedor Urvanov <fedor@urvanov.ru>
2024-05-12 08:48:41 +0300

    PetDao code style for the book

--- src/main/java/ru/urvanov/virtualpets/server/dao/PetDao.java
+++ src/main/java/ru/urvanov/virtualpets/server/dao/PetDao.java
@@ -22,84 +22,89 @@ import ru.urvanov.virtualpets.server.dao.domain.PetJournalEntry_;
 import ru.urvanov.virtualpets.server.dao.domain.Pet_;
 
 @Transactional(readOnly = true)
-public interface PetDao
-        extends JpaRepository<Pet, Integer>, JpaSpecificationExecutor<Pet> {
+public interface PetDao extends JpaRepository<Pet, Integer>,
+        JpaSpecificationExecutor<Pet> {
 
     Optional<Pet> findFullById(Integer id);
 
     List<Pet> findByUserId(Integer userId);
-    
+
     long countByUserId(Integer userId);
-    
+
     default long getPetNewJournalEntriesCount(Integer petId) {
-        return this.count(PetDao.getPetNewJournalEntriesSpecification(petId));
+        return this.count(
+                PetDao.getPetNewJournalEntriesSpecification(petId));
     }
-    
-    private static Specification<Pet> getPetNewJournalEntriesSpecification(Integer petId) {
-        return (rootPet,
-                    criteriaQuery,
-                    criteriaBuilder) -> {
-                MapJoin<Pet, JournalEntryId, PetJournalEntry> joinPetJournalEntries = rootPet
-                        .join(Pet_.journalEntries, JoinType.LEFT);
-                return criteriaBuilder.and(
-                        criteriaBuilder
-                                .and(criteriaBuilder.equal(
-                                        joinPetJournalEntries
-                                                .get(PetJournalEntry_.readed),
-                                        false)),
-                        criteriaBuilder.equal(rootPet.get(Pet_.id), petId));
-                    };
+
+    private static Specification<Pet> getPetNewJournalEntriesSpecification(
+            Integer petId) {
+        return (rootPet, criteriaQuery, criteriaBuilder) -> {
+            MapJoin<Pet, JournalEntryId, PetJournalEntry> joinPetJournalEntries = rootPet
+                    .join(Pet_.journalEntries, JoinType.LEFT);
+            return criteriaBuilder.and(
+                    criteriaBuilder
+                            .and(criteriaBuilder.equal(
+                                    joinPetJournalEntries.get(
+                                            PetJournalEntry_.readed),
+                                    false)),
+                    criteriaBuilder.equal(rootPet.get(Pet_.id), petId));
+        };
     }
 
     default Page<Pet> findLastCreatedPets(int page, int pageSize) {
-        return this.findAll(PageRequest.of(page, pageSize, Sort.by("createdDate").descending()));
+        return this.findAll(PageRequest.of(page, pageSize,
+                Sort.by("createdDate").descending()));
     }
-    
+
     @Query("from Pet p left outer join p.buildingMaterials bm where p.id = ?1")
     Optional<Pet> findByIdWithBuildingMaterials(Integer id);
-    
+
     @EntityGraph("pet.buildingMaterials")
     @Query(name = "Pet.findById")
     Optional<Pet> findByIdWithFullBuildingMaterials(Integer id);
-    
+
     @EntityGraph("pet.foods")
     @Query(name = "Pet.findById")
     Optional<Pet> findByIdWithFullFoods(Integer id);
-    
+
     @EntityGraph("pet.drinks")
     @Query(name = "Pet.findById")
     Optional<Pet> findByIdWithFullDrinks(Integer id);
-    
+
     @EntityGraph("pet.cloths")
     @Query(name = "Pet.findById")
     Optional<Pet> findByIdWithFullCloths(Integer id);
-    
+
     @EntityGraph("pet.books")
     @Query(name = "Pet.findById")
     Optional<Pet> findByIdWithFullBooks(Integer id);
-    
+
     @EntityGraph("pet.journalEntriesAndAchievements")
     @Query(name = "Pet.findById")
     Optional<Pet> findByIdWithJournalEntriesAndAchievements(Integer id);
-    
+
     @EntityGraph("pet.booksAndJournalEntriesAndBuildingMaterials")
     @Query(name = "Pet.findById")
-    Optional<Pet> findByIdWithBooksAndJournalEntriesAndBuildingMaterials(Integer id);
-    
+    Optional<Pet> findByIdWithBooksAndJournalEntriesAndBuildingMaterials(
+            Integer id);
+
     @EntityGraph("pet.foodsAndJournalEntriesAndBuildingMaterials")
     @Query(name = "Pet.findById")
-    Optional<Pet> findByIdWithFoodsAndJournalEntriesAndBuildingMaterials(Integer id);
-    
+    Optional<Pet> findByIdWithFoodsAndJournalEntriesAndBuildingMaterials(
+            Integer id);
+
     @EntityGraph("pet.drinksAndJournalEntriesAndBuildingMaterialsAndAchievements")
     @Query(name = "Pet.findById")
     Optional<Pet> findByIdWithDrinksAndJournalEntriesAndBuildingMaterialsAndAchievements(
             Integer id);
-    
+
     @EntityGraph("pet.drinksAndJournalEntriesAndAchievements")
     @Query(name = "Pet.findById")
-    Optional<Pet> findByIdWithDrinksAndJournalEntriesAndAchievements(Integer id);
-    
+    Optional<Pet> findByIdWithDrinksAndJournalEntriesAndAchievements(
+            Integer id);
+
     @EntityGraph("pet.foodsAndJournalEntriesAndAchievements")
     @Query(name = "Pet.findById")
-    Optional<Pet> findByIdWithFoodsJournalEntriesAndAchievements(Integer id);
+    Optional<Pet> findByIdWithFoodsJournalEntriesAndAchievements(
+            Integer id);
 }
