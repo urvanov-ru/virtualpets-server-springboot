e59d0d33ce26c3e77d13914495ff347c38ca68ef
Fedor Urvanov <fedor@urvanov.ru>
2024-07-17 17:19:14 +0300

    HomeController стиль кодирования для книги

--- src/main/java/ru/urvanov/virtualpets/server/controller/site/HomeController.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/site/HomeController.java
@@ -1,7 +1,5 @@
 package ru.urvanov.virtualpets.server.controller.site;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -25,13 +23,6 @@ public class HomeController {
     public String home(Locale locale, Model model,
             HttpServletRequest request) {
         logger.info("Welcome home! The client locale is {}.", locale);
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(
-                DateFormat.LONG, DateFormat.LONG, locale);
-
-        String formattedDate = dateFormat.format(date);
-
-        model.addAttribute("serverTime", formattedDate);
 
         return "home";
     }
