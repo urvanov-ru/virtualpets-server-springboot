37cb7a91092674f6449601307f23375ffd4c236f
Fedor Urvanov <fedor@urvanov.ru>
2024-07-17 17:23:03 +0300

    HomeController стиль кодирования для книги

--- src/main/java/ru/urvanov/virtualpets/server/controller/site/HomeController.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/site/HomeController.java
@@ -5,11 +5,8 @@ import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("site")
@@ -20,8 +17,7 @@ public class HomeController {
 
     @RequestMapping(value = "/home",
             method = RequestMethod.GET)
-    public String home(Locale locale, Model model,
-            HttpServletRequest request) {
+    public String home(Locale locale) {
         logger.info("Welcome home! The client locale is {}.", locale);
         return "home";
     }
