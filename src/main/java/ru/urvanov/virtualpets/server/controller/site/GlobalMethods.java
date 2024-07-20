40cc82545becd53760cf085390cc05bca8f0c919
Fedor Urvanov <fedor@urvanov.ru>
2024-07-20 13:09:31 +0300

    Стиль кодирования для книги

--- src/main/java/ru/urvanov/virtualpets/server/controller/site/GlobalMethods.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/site/GlobalMethods.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class GlobalMethods {
     
+    /**
+     * Добавляет атрибут menu_play_url к модели всех контроллеров. 
+     * Атрибут menu_play_url используется фрагментом
+     * src/main/resources/templates/fragments/menu.html
+     * в качестве ссылки на клиентскую часть игры.
+     * @param playUrl Ссылка на клиентскую часть игры.
+     * @return Значение для атрибута menu_play_url
+     */
     @ModelAttribute("menu_play_url")
     public String menuPlayUrl(
             @Value("${virtualpets-server-springboot.play.url}")
