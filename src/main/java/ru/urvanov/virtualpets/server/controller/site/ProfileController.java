e10f0cbd3318fa2831ff9bdffdd49e6140a5c33f
Fedor Urvanov <fedor@urvanov.ru>
2024-08-11 09:50:19 +0300

    Исправление ProfileController после последних изменений

--- src/main/java/ru/urvanov/virtualpets/server/controller/site/ProfileController.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/site/ProfileController.java
@@ -5,15 +5,17 @@ import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
+import ru.urvanov.virtualpets.server.auth.UserDetailsImpl;
 import ru.urvanov.virtualpets.server.service.UserService;
-import ru.urvanov.virtualpets.server.service.domain.UserPetDetails;
 import ru.urvanov.virtualpets.server.service.domain.UserProfile;
+import ru.urvanov.virtualpets.server.service.exception.UserNotFoundException;
 
 @Controller
 @RequestMapping("site/user/profile")
@@ -23,17 +25,22 @@ public class ProfileController {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private UserPetDetails userPetDetails;
     
     /**
-     * Получает данные от социальной сети и сохраняет в пользователях игры.
+     * Получает данные от социальной сети и сохраняет
+     * в пользователях игры.
+     * @throws UserNotFoundException 
      */
     @GetMapping
-    public String home(Locale locale, Model model, HttpServletRequest request) {
+    public String home(
+            Locale locale,
+            Model model,
+            HttpServletRequest request,
+            @AuthenticationPrincipal UserDetailsImpl userDetailsImpl)
+                    throws UserNotFoundException {
         logger.info("Welcome home! The client locale is {}.", locale);
-        UserProfile userProfile = userService.getProfile(userPetDetails);
+        UserProfile userProfile = userService.getProfile(
+                userDetailsImpl.getUserId());
         model.addAttribute("userProfile", userProfile);
         return "user/profile";
     }
