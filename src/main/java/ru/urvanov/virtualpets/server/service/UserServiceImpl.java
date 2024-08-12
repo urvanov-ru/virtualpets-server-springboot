b00cde76bd390f10ac29103828e9f925ecd29ec8
Fedor Urvanov <fedor@urvanov.ru>
2024-08-12 12:59:21 +0300

    Стиль кодирования для книги

--- src/main/java/ru/urvanov/virtualpets/server/service/UserServiceImpl.java
+++ src/main/java/ru/urvanov/virtualpets/server/service/UserServiceImpl.java
@@ -165,9 +165,13 @@ public class UserServiceImpl implements UserService, UserApiService  {
     }
 
     @Override
-    @PreAuthorize("hasRole('ADMIN') && (#userAccessRights.id ne principal.userId)")
+    @PreAuthorize("""
+            hasRole('ADMIN') \
+            && (#userAccessRights.id ne principal.userId)
+            """)
     @Transactional(rollbackFor = ServiceException.class)
-    public UserAccessRights saveUserAccessRights(@P("userAccessRights") UserAccessRights userAccessRights)
+    public UserAccessRights saveUserAccessRights(
+            @P("userAccessRights") UserAccessRights userAccessRights)
             throws UserNotFoundException {
         User user = userDao.findById(userAccessRights.getId())
                 .orElseThrow(() -> new UserNotFoundException(
