18b87e5c736a4e12d251ac2020ce261c3e2fcd7e
Fedor Urvanov <fedor@urvanov.ru>
2024-08-14 00:17:03 +0300

    Документирование OpenBoxNewbieResult для OpenAPI

--- src/main/java/ru/urvanov/virtualpets/server/controller/api/domain/OpenBoxNewbieResult.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/api/domain/OpenBoxNewbieResult.java
@@ -6,6 +6,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import ru.urvanov.virtualpets.server.dao.domain.BuildingMaterialId;
 
 @Schema(description = "Добыча из лутбокса")
-public record OpenBoxNewbieResult(int index,
+public record OpenBoxNewbieResult(
+        
+        @Schema(
+                description = "Индекс лутбокса. Начинается с 0.",
+                example = "0")
+        int index,
+        
+        @Schema(
+                description = "Полученные ресурсы",
+                example = """
+                        {"TIMBER": 2, "STONE": 3}
+                        """)
         Map<BuildingMaterialId, Integer> buildingMaterials) {
 };
