76df764f92808caa4010e3073dfbdb743ea67248
Fedor Urvanov <fedor@urvanov.ru>
2024-08-13 23:54:33 +0300

    produces application/json

--- src/main/java/ru/urvanov/virtualpets/server/controller/api/HiddenObjectsController.java
+++ src/main/java/ru/urvanov/virtualpets/server/controller/api/HiddenObjectsController.java
@@ -25,7 +25,8 @@ import ru.urvanov.virtualpets.server.service.exception.ServiceException;
 
 @RestController
 @RequestMapping(value = "api/v1/HiddenObjectsService",
-        consumes = MediaType.APPLICATION_JSON_VALUE)
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
 public class HiddenObjectsController extends ControllerBase {
     
     @Autowired
