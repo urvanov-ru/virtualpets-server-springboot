f08f189d00ee13477893715c23a96dd780242fa7
Fedor Urvanov <fedor@urvanov.ru>
2024-08-31 12:11:02 +0300

    TestNG + Mockito пример для книги

---
+++ src/test/java/ru/urvanov/virtualpets/server/service/UserServiceTestNgTest.java
@@ -0,0 +1,83 @@
+package ru.urvanov.virtualpets.server.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import ru.urvanov.virtualpets.server.controller.api.domain.RefreshUsersOnlineResult;
+import ru.urvanov.virtualpets.server.controller.api.domain.UserInfo;
+import ru.urvanov.virtualpets.server.dao.UserDao;
+import ru.urvanov.virtualpets.server.dao.domain.User;
+import ru.urvanov.virtualpets.server.service.domain.UserPetDetails;
+import ru.urvanov.virtualpets.server.service.exception.ServiceException;
+
+@Listeners(MockitoTestNGListener.class)
+public class UserServiceTestNgTest {
+
+    private static final Integer USER_ID = 1;
+
+    private static final Integer PET_ID = 2;
+
+    private static final String USER_FULL_NAME = "Иванов Иван";
+
+    @Mock
+    private UserDao userDao;
+    
+    private ZoneId zoneId = ZoneId.of("Europe/Moscow");
+    
+    @Spy
+    private Clock clock = Clock.fixed(ZonedDateTime
+            .of(LocalDate.of(2024, 8, 14), LocalTime.of(16, 58), zoneId)
+            .toInstant(), zoneId);
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    void getUsersOnline() throws ServiceException {
+        // Подготовка тестовых данных
+        UserPetDetails userPetDetails = new UserPetDetails(USER_ID,
+                PET_ID);
+        List<User> usersOnline = new ArrayList<>();
+        User user1 = new User();
+        user1.setId(USER_ID);
+        user1.setName(USER_FULL_NAME);
+        usersOnline.add(user1);
+        
+        
+        // Настройка mock-объектов
+        OffsetDateTime activeAfterDate = OffsetDateTime.now(clock)
+                .minusMinutes(5);
+        when(userDao.findByActiveDateAfter(eq(activeAfterDate)))
+                        .thenReturn(usersOnline);
+        
+        // Вызов тестируемого метода
+        RefreshUsersOnlineResult actual = userService
+                .getUsersOnline(userPetDetails);
+        
+        // Проверка результата
+        assertNotNull(actual.users());
+        assertEquals(1, actual.users().size());
+        UserInfo actualUser0 = actual.users().get(0);
+        assertEquals((Integer) USER_ID, (Integer) actualUser0.id());
+        assertEquals(USER_FULL_NAME, actualUser0.name());
+    }
+
+}
