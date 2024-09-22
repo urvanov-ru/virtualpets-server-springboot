2cfb3aec58d6e853d2c5ddba5bd0c66a13e7a594
Fedor Urvanov <fedor@urvanov.ru>
2024-09-22 20:12:10 +0300

    Стиль кодирования для книги.

--- src/main/java/ru/urvanov/virtualpets/server/config/ClockConfig.java
+++ src/main/java/ru/urvanov/virtualpets/server/config/ClockConfig.java
@@ -5,9 +5,19 @@ import java.time.Clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * конфигурация {@link Clock}
+ */
 @Configuration
 public class ClockConfig {
     
+    /**
+     * Классы пакета {@link java.time} используют этот экземпляр Clock
+     * при создании экземпляров с текущей датой и временем. Подобная
+     * практика позволяет подставить другой Clock, возвращающий всегда
+     * фиксированное значение, для тестов.
+     * @return Экземпляр {@link Clock}
+     */
     @Bean
     public Clock clock() {
         return Clock.systemDefaultZone();
